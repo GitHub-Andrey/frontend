f627aae3e021b409911dafd4d6f72a28aa234405
GitHub-Andrey <78257276+GitHub-Andrey@users.noreply.github.com>
2023-12-19 10:27:32 +0100

    1
    1

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ const myImage = document.querySelector('.logo');
 /* Функция для смены картинки при клике по ней */
 
 myImage.onclick = () => {
-    const mySrc = myImage.getAttribute("src");
+	const mySrc = myImage.getAttribute("src");
 
-    if (mySrc == "images/1.jpg") {
-        myImage.setAttribute("src", "images/2.png");
-    } else {
-        myImage.setAttribute("src", "images/1.jpg");
-    }
+	if (mySrc == "images/1.jpg") {
+		myImage.setAttribute("src", "images/2.png");
+	} else {
+		myImage.setAttribute("src", "images/1.jpg");
+	}
 }
 
 
@@ -27,25 +27,25 @@ const myHeading = document.querySelector('h1')
 // Функция вызывается нажатием кнопки, спрашивает имя пользователя и создаёт приветствие
 
 function setUserName() {
-    const myName = prompt("Пожалуйста введите Ваше имя.");
+	const myName = prompt("Пожалуйста введите Ваше имя.");
 
-    localStorage.setItem("name", myName);
-    myHeading.textContent = "Добро пожаловать к нам на огонёк, " + myName + '!';
+	localStorage.setItem("name", myName);
+	myHeading.textContent = "Добро пожаловать к нам на огонёк, " + myName + '!';
 }
 
 // Если не сохранено ни одно имя пользователя, то вызывается функция
 
 if (!localStorage.getItem("name")) {
-    setUserName();
+	setUserName();
 } else {
-    // Вызывается имя из памяти браузера, если оно сохранено в памяти
+	// Вызывается имя из памяти браузера, если оно сохранено в памяти
 
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = "Добро пожаловать к нам на огонёк, " + storedName + '!';
+	const storedName = localStorage.getItem("name");
+	myHeading.textContent = "Добро пожаловать к нам на огонёк, " + storedName + '!';
 }
 
 // Нажатие на кнопку запускает функцию для принудительной смены имени пользователя
 
 myButton.onclick = function () {
-    setUserName();
+	setUserName();
 };
